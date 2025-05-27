a08b69d825896bf53f3fdaa7193b338af5e48f7b
Arslan Charyyev <88978221+arslan-charyyev@users.noreply.github.com>
2025-05-27 06:32:12 +0500

    Added `TIKTOK_ENABLED` and `INSTAGRAM_ENABLED` config options

--- src/core/config.ts
+++ src/core/config.ts
@@ -13,38 +13,12 @@ const intCsv = z
   .pipe(z.array(z.number().int()));
 
 const Config = z.object({
-  BOT_TOKEN: z
-    .string()
-    .describe(
-      "A required bot token, which can be obtained from the https://t.me/BotFather",
-    ),
-  LOG_LEVEL: z
-    .enum(zodEnum(LogLevelNames))
-    .default("DEBUG")
-    .describe("Log level. Source: https://jsr.io/@std/log/0.224.7/levels.ts"),
-  WITH_CAPTION: bool
-    .default("true")
-    .describe("Send media with title/description as caption"),
-  SEND_AS_REPLY: bool
-    .default("true")
-    .describe("Send media as a reply or as a regular message"),
-  SEND_ERRORS: bool
-    .default("true")
-    .describe("Send errors to the corresponding chat"),
-  SHOW_CAPTION_ABOVE_MEDIA: bool
-    .default("false")
-    .describe("Send caption above media (true), or below (false)"),
-  REPORT_ERRORS_TO: intCsv
+  BOT_ADMINS: intCsv
     .default("")
     .describe(
-      "A comma-separated list of user or chat IDs that will receive notifications about any errors",
+      "A comma-separated list of user or chat IDs that can change bot settings",
     ),
-  WHITELIST: intCsv
-    .default("")
-    .describe(
-      "A comma-separated list of user or chat IDs that are allowed to make requests. " +
-        "If empty, then no restrictions apply.",
-    ),
+
   BOT_API_ROOT: z
     .string()
     .default("")
@@ -52,21 +26,66 @@ const Config = z.object({
       "A URL to a self-hosted Telegram Bot API server instance. " +
         "Read more: https://grammy.dev/guide/api#running-a-local-bot-api-server",
     ),
-  BOT_ADMINS: intCsv
-    .default("")
+
+  BOT_TOKEN: z
+    .string()
     .describe(
-      "A comma-separated list of user or chat IDs that can change bot settings",
+      "A required bot token, which can be obtained from the https://t.me/BotFather",
     ),
+
   DATA_DIR: z
     .string()
     .default("")
     .describe(
       "A path to directory for storing app data, such as database files",
     ),
+
+  INSTAGRAM_ENABLED: bool
+    .default("true")
+    .describe("Enables support for downloading Instagram media"),
+
+  LOG_LEVEL: z
+    .enum(zodEnum(LogLevelNames))
+    .default("DEBUG")
+    .describe("Log level. Source: https://jsr.io/@std/log/0.224.7/levels.ts"),
+
+  REPORT_ERRORS_TO: intCsv
+    .default("")
+    .describe(
+      "A comma-separated list of user or chat IDs that will receive notifications about any errors",
+    ),
+
+  SEND_AS_REPLY: bool
+    .default("true")
+    .describe("Send media as a reply or as a regular message"),
+
+  SEND_ERRORS: bool
+    .default("true")
+    .describe("Send errors to the corresponding chat"),
+
+  SHOW_CAPTION_ABOVE_MEDIA: bool
+    .default("false")
+    .describe("Send caption above media (true), or below (false)"),
+
   TEST_INSTAGRAM_COOKIE: z
     .string()
     .default("")
     .describe("Instagram cookies to be used for testing"),
+
+  TIKTOK_ENABLED: bool
+    .default("true")
+    .describe("Enables support for downloading TikTok media"),
+
+  WHITELIST: intCsv
+    .default("")
+    .describe(
+      "A comma-separated list of user or chat IDs that are allowed to make requests. " +
+        "If empty, then no restrictions apply.",
+    ),
+
+  WITH_CAPTION: bool
+    .default("true")
+    .describe("Send media with title/description as caption"),
 });
 
 const env = {
